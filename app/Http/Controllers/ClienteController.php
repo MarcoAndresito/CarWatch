2e3aa32c7d5e62261432ef121d9a60f564fca55e
Andres Vasquez <myapps@myapps.com.bo>
2022-04-18 21:35:45 -0400

    add controller and routes

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    function index()
+    {
+    }
+
+    function edit()
+    {
+    }
+
+    function delete()
+    {
+    }
+
+    function new()
+    {
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,32 +4,13 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/cliente', [App\Http\Controllers\ClienteController::class, 'index'])->name('cliente');
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::put('/cliente/editar', [\App\Http\Controllers\ClienteController::class, 'edit'])->name('cliente');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
-});
+Route::delete('/cliente/eliminar', [\App\Http\Controllers\ClienteController::class, 'delete'])->name('cliente');
+
+Route::post('/cliente/nuevo', [\App\Http\Controllers\ClienteController::class, 'new'])->name('cliente');
+
+
+Route::resource('Auto',\App\Http\Controllers\AutoController::class);
